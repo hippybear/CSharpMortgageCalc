40af3a283ac04b2ef118295c5b4a7fc8d826a342
hippybear <creekhippybear@gmail.com>
2017-07-18 12:43:19 -0700

    First push

---
+++ MortgageCalculator/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MortgageCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LoanAmount = new System.Windows.Forms.Label();
+            this.Interest = new System.Windows.Forms.Label();
+            this.Years = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(47, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate Payments";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(164, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // LoanAmount
+            // 
+            this.LoanAmount.AutoSize = true;
+            this.LoanAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoanAmount.Location = new System.Drawing.Point(12, 17);
+            this.LoanAmount.Name = "LoanAmount";
+            this.LoanAmount.Size = new System.Drawing.Size(134, 24);
+            this.LoanAmount.TabIndex = 2;
+            this.LoanAmount.Text = "Loan Amount";
+            // 
+            // Interest
+            // 
+            this.Interest.AutoSize = true;
+            this.Interest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Interest.Location = new System.Drawing.Point(12, 88);
+            this.Interest.Name = "Interest";
+            this.Interest.Size = new System.Drawing.Size(78, 24);
+            this.Interest.TabIndex = 4;
+            this.Interest.Text = "Interest";
+            // 
+            // Years
+            // 
+            this.Years.AutoSize = true;
+            this.Years.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Years.Location = new System.Drawing.Point(12, 52);
+            this.Years.Name = "Years";
+            this.Years.Size = new System.Drawing.Size(63, 24);
+            this.Years.TabIndex = 5;
+            this.Years.Text = "Years";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(84, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 22);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "0.00";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 269);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Years);
+            this.Controls.Add(this.Interest);
+            this.Controls.Add(this.LoanAmount);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label LoanAmount;
+        private System.Windows.Forms.Label Interest;
+        private System.Windows.Forms.Label Years;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MortgageCalculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------
+//   Description:    Simple mortgage calculator
+//   Author:         @hippybear     Date: 7/17/17
+//   Notes:          More features coming feel free to email me
+//                   to request more...
+//
+//   Payments = Loan ( intRate(1 + intRate)^months ) / ( (1 + intRate)^months - 1 )
+// 
+//-----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MortgageCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+//   Payments = Loan ( intRate(1 + intRate)^months ) / ( (1 + intRate)^months - 1 )
+            var loan = 0;
+            var loanAmount = int.TryParse(textBox1.Text, out loan);
+
+            var years = 0;
+            var monthsText = int.TryParse(textBox2.Text, out years);
+
+            var interestRate = 0;
+            var interestRateText = int.TryParse(textBox3.Text, out interestRate);
+            var months = years / 12.0;
+
+            var payments = (interestRate * Math.Pow((1 + interestRate), months)) / (Math.Pow((1 + interestRate), months) - 1);
+
+            textBox4.Text = payments.ToString();
+        }
+    }
+}
